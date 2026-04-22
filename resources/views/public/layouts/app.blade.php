9eb9ad3531196416252ef34a449fc44b1c22c539
fitra <imaniar.fitraa@gmail.com>
2026-04-22 18:28:14 +0700

    push

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -5,6 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    @if (isset($news))
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="article">
+        <meta property="og:url" content="{{ url()->current() }}">
+        <meta property="og:title" content="{{ $news->title }}">
+        <meta property="og:description" content="{{ Str::limit(strip_tags($news->content), 155) }}">
+        <meta property="og:image" content="{{ asset('storage/' . $news->thumbnail) }}">
+        <meta property="og:image:width" content="1200">
+        <meta property="og:image:height" content="630"> <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="{{ url()->current() }}">
+        <meta property="twitter:title" content="{{ $news->title }}">
+        <meta property="twitter:description" content="{{ Str::limit(strip_tags($news->content), 155) }}">
+        <meta property="twitter:image" content="{{ asset('storage/' . $news->thumbnail) }}">
+    @endif
     <title>@yield('title') - DISKATAN</title>
     <link rel="icon" type="image" href="{{ asset('img/logo/kuningan.png') }}" />
 
